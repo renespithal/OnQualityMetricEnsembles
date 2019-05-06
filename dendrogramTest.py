7c68ac6ebe84f570a4354f6d67d68257a9fbabc0
Rene Nespithal <rene.nespithal@gmail.com>
2019-05-06 20:03:20 +0200

    Create dendrogramTest.py

---
+++ dendrogramTest.py
@@ -0,0 +1,24 @@
+# Importing the libs
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Test-Data
+Data = [[1, 2],
+ [2, 2],
+ [3, 4],
+ [2, 3],
+ [1, 1],
+ [4, 5],
+ [5, 6],
+ [4, 7],
+ [3, 2],
+ [6, 3]]
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as hier
+dendrogram = hier.dendrogram(hier.linkage(Data, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Index')
+plt.ylabel('Distance')
+plt.show()
