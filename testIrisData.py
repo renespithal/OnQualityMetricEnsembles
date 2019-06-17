f4e059b06c1807cfcd4d3e481dc48ea720e2d35f
Rene Nespithal <rene.nespithal@gmail.com>
2019-06-18 01:18:58 +0200

    Update testIrisData.py

--- testIrisData.py
+++ testIrisData.py
@@ -1,4 +1,4 @@
-# Code source: Gaël Varoquaux
+# Code source: GaÃ«l Varoquaux
 # Modified for documentation by Jaques Grobler
 # License: BSD 3 clause
 
@@ -33,17 +33,56 @@ plt.yticks(())
 from scipy.spatial.distance import pdist
 import scipy.cluster.hierarchy as hier
 import numpy as np
+
 dendrogram = hier.dendrogram(hier.linkage(X, method = 'ward'))
 plt.title('Dendrogram')
 plt.xlabel('Index')
 plt.ylabel('Distance')
 plt.show()
 
+""" 
+trying to generate adjac from linkage
+
+"""
+
+
+
+
 # Cutting the dendrogram with cut_tree
 from scipy import cluster
 Z = cluster.hierarchy.ward(X)
 cutree = cluster.hierarchy.cut_tree(Z,None,3)
-print(cutree)
+AZ = cluster.hierarchy.ward(cutree)
+
+
+dendrogram = hier.dendrogram(hier.linkage(AZ, method = 'ward'))
+plt.title('Dendrogram2')
+plt.xlabel('Index')
+plt.ylabel('Distance')
+plt.show()
+
+import networkx as nx
+# Create a networkx graph object
+new_graph = nx.Graph() 
+
+adjac = [(0,0)]
+for bb in AZ:
+    adjac.append((tuple(bb[:-2])))
+    print(adjac)
+
+
+new_graph.add_edges_from(adjac)
+ 
+# Draw the resulting graph
+nx.draw(new_graph, with_labels=True, font_weight='bold')
+
+
+"""
+
+END
+
+"""
+
 
 # Cutting the dendrogram fcluster
 distanceMatrix = pdist(X)
