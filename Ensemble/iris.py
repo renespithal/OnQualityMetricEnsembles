792fae6bd3f81e2e715ee6e844862f8c80608d3d
Rene Nespithal <rene.nespithal@gmail.com>
2019-07-02 00:47:53 +0200

    Update iris.py

--- Ensemble/iris.py
+++ Ensemble/iris.py
@@ -1,6 +1,4 @@
-# Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
+# Iris Data from sklearn
 
 import matplotlib.pyplot as plt
 from sklearn import datasets
@@ -33,85 +31,85 @@ plt.show()
 distanceMatrix = pdist(Data)
 Z = hier.linkage(distanceMatrix, method='ward')
 
-# cut dendrogram at certain distance
+# cut dendrogram at certain distances
+cutDistance = [2,3]
 
-cutDistance = [0,1,2,3,4,5,6,7,8]
-a = 2
-print(cutDistance[a])
-fc = hier.fcluster(Z, cutDistance[a], criterion='distance')
-clusterL=[[]]
-print("\n")
-print("Ensemble Scores for Cutsize: ", cutDistance[a])
-print("\n")
-print("Number of Clusters: ", max(fc))
-print("\n")
-# print the clusters 
-for k in range(1,max(fc)+1):
-    print("Flat cluster",k,": ", list(np.where(fc==k)[0]))
-    clusterL.append(list(np.where(fc==k)[0]))
-
-# make list from list of list cluster
-flat_list = []
-for sublist in clusterL:
-    for item in sublist:
-        flat_list.append(item)
-print("Flat cluster list: ", flat_list)
-print("\n")
-# Create a networkx graph object
-new_graph = nx.Graph() 
-
-adjac = [(0,0)] # should not make any difference
-n = len(Z)
-# make adjacency matrix from linkage matrix
-for bb in Z:
-    # connection to the next node/cluster
-    n=n+1
-    x,y = (tuple(bb[:-2]))
-    adjac.append(tuple([x,n]))
-    adjac.append(tuple([y,n]))
-    # print(adjac)
-#print(adjac)
-#print(Z)
-
-new_graph.add_edges_from(adjac)
- 
-# Draw the resulting graph
-nx.draw(new_graph, with_labels=True, font_weight='bold')
-
-# Modularity Communities
-barbMod = list(greedy_modularity_communities(new_graph))
-
-# Modularity Score
-barbModScore =  qu.modularity(new_graph, barbMod)
-
-# edge_betweenness_centrality
-barbedgeBetweenness=  nx.edge_betweenness_centrality(new_graph,None,False)
-barbaverageEdge = sum(barbedgeBetweenness.values())/len(barbedgeBetweenness)
-barbtotalEdge = sum(barbedgeBetweenness.values())
-print("\n")
-print("\n")
-print("Scores for Cut Distance: ", cutDistance[a])
-print("\n")
-# print sets of nodes, one for each community.
-print("Communities: ", barbMod)
-print("\n")
-# Modularity Score
-print("Modularity: ", barbModScore)
-print("\n")
-# Conductance Score
-sumOfCond = []
-
-for i in range(len(clusterL)):
-    if clusterL[i] == []:
-        # catch Division by zero
-        continue
-    # calculate Cond for current Cluster in list of Clusters 
-    currentCond = conductance(new_graph,clusterL[i])
-    sumOfCond.append(currentCond)
-    print("Conductance for: ", clusterL[i], " = ", currentCond)
-overallCond = min(sumOfCond)
-print("Overall Conductance: ", overallCond)
-print("\n")
-
-# edge_betweenness_centrality
-print("Edge Betweenness Centrality Score: ", barbaverageEdge)
+# Calculate score and draw graph for each cut distance
+for a in range (len(cutDistance)):
+    fc = hier.fcluster(Z, cutDistance[a], criterion='distance')
+    clusterL=[[]]
+    print("\n")
+    print("Ensemble Scores for Cutsize: ", cutDistance[a])
+    print("\n")
+    print("Number of Clusters: ", max(fc))
+    print("\n")
+    # print the clusters 
+    for k in range(1,max(fc)+1):
+        print("Flat cluster",k,": ", list(np.where(fc==k)[0]))
+        clusterL.append(list(np.where(fc==k)[0]))
+    
+    # make list from list of list cluster
+    flat_list = []
+    for sublist in clusterL:
+        for item in sublist:
+            flat_list.append(item)
+    print("Flat cluster list: ", flat_list)
+    print("\n")
+    # Create a networkx graph object
+    new_graph = nx.Graph() 
+    
+    adjac = [(0,0)] # should not make any difference
+    n = len(Z)
+    # make adjacency matrix from linkage matrix
+    for bb in Z:
+        # connection to the next node/cluster
+        n=n+1
+        x,y = (tuple(bb[:-2]))
+        adjac.append(tuple([x,n]))
+        adjac.append(tuple([y,n]))
+        # print(adjac)
+    #print(adjac)
+    #print(Z)
+    
+    new_graph.add_edges_from(adjac)
+     
+    # Draw the resulting graph
+    nx.draw(new_graph, with_labels=True, font_weight='bold')
+    
+    # Modularity Communities
+    barbMod = list(greedy_modularity_communities(new_graph))
+    
+    # Modularity Score
+    barbModScore =  qu.modularity(new_graph, barbMod)
+    
+    # edge_betweenness_centrality
+    barbedgeBetweenness=  nx.edge_betweenness_centrality(new_graph,None,False)
+    barbaverageEdge = sum(barbedgeBetweenness.values())/len(barbedgeBetweenness)
+    barbtotalEdge = sum(barbedgeBetweenness.values())
+    print("\n")
+    print("\n")
+    print("Scores for Cut Distance: ", cutDistance[a])
+    print("\n")
+    # print sets of nodes, one for each community.
+    print("Communities: ", barbMod)
+    print("\n")
+    # Modularity Score
+    print("Modularity: ", barbModScore)
+    print("\n")
+    # Conductance Score
+    sumOfCond = []
+    
+    for i in range(len(clusterL)):
+        if clusterL[i] == []:
+            # catch Division by zero
+            continue
+        # calculate Cond for current Cluster in list of Clusters 
+        currentCond = conductance(new_graph,clusterL[i])
+        sumOfCond.append(currentCond)
+        print("Conductance for: ", clusterL[i], " = ", currentCond)
+    overallCond = min(sumOfCond)
+    print("Overall Conductance: ", overallCond)
+    print("\n")
+    
+    # edge_betweenness_centrality
+    print("Edge Betweenness Centrality Score: ", barbaverageEdge)
