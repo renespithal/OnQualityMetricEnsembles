8afcd3eda1c8997b8ae68e54f88ad2032bcba2ab
Rene Nespithal <rene.nespithal@gmail.com>
2019-07-02 13:24:13 +0200

    Update iris.py

--- Ensemble/iris.py
+++ Ensemble/iris.py
@@ -1,21 +1,22 @@
-# Iris Data from sklearn
-
 import matplotlib.pyplot as plt
 from sklearn import datasets
 import numpy as np
 import networkx as nx
 from networkx.algorithms.community import greedy_modularity_communities
 from networkx.algorithms.cuts import conductance
+from networkx.algorithms.cuts import volume
 from networkx.algorithms.community import quality as qu
 
-# import some data to play with
+# import Iris data from sklearn
 iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
+# we only take the first two features.
+X = iris.data[:, :2]  
 x = X[:, 0]
 y = X[:, 1]
 plt.scatter(x, y)
 plt.show()
 Data = X[:, :2]
+
 
 # Creating a dendrogram
 from scipy.spatial.distance import pdist
@@ -27,17 +28,46 @@ plt.ylabel('Distance')
 plt.show()
 
 
-# Cutting the dendrogram
+# Creating Linkage Matrix Z
 distanceMatrix = pdist(Data)
 Z = hier.linkage(distanceMatrix, method='ward')
 
-# cut dendrogram at certain distances
+
+# Create a networkx graph before any cuts
+original_graph = nx.Graph() 
+originalAdjac = []
+n = len(Z)
+# make adjacency matrix from linkage matrix
+for bb in Z:
+    # connection to the next node/cluster
+    # n = cluster ID
+    n=n+1
+    if bb[2] == 0:
+        continue
+    x,y = (tuple(bb[:-2]))
+    originalAdjac.append(tuple([x,n]))
+    originalAdjac.append(tuple([y,n]))
+
+original_graph.add_edges_from(originalAdjac)
+
+
+# cut dendrogram at distances where clusters were formed
+cutIntervals = []
+for xs in Z:
+    cutIntervals.append(xs[2])
+rCutIntervals = np.around(cutIntervals, decimals=1)
+intersectIntervals = sorted(set(rCutIntervals))
+
+
+# cut dendrogram at equi distances
+## cutDistance = list(range(int(round(max(intersectIntervals)))+1))
 cutDistance = [3]
+
 
 # Calculate score and draw graph for each cut distance
 for a in range (len(cutDistance)):
     fc = hier.fcluster(Z, cutDistance[a], criterion='distance')
-    clusterL=[[]]
+    clusterL=[]
     print("\n")
     print("Ensemble Scores for Cutsize: ", cutDistance[a])
     print("\n")
@@ -45,7 +75,7 @@ for a in range (len(cutDistance)):
     print("\n")
     # print the clusters 
     for k in range(1,max(fc)+1):
-        print("Flat cluster",k,": ", list(np.where(fc==k)[0]))
+        print("Cluster",k,": ", list(np.where(fc==k)[0]))
         clusterL.append(list(np.where(fc==k)[0]))
     
     # make list from list of list cluster
@@ -53,26 +83,22 @@ for a in range (len(cutDistance)):
     for sublist in clusterL:
         for item in sublist:
             flat_list.append(item)
-    print("Flat cluster list: ", flat_list)
-    print("\n")
+    #print("Flat cluster list: ", flat_list)
+    #print("\n")
     # Create a networkx graph object
     new_graph = nx.Graph() 
     
-    adjac = [(0,0)] # should not make any difference
+    adjac = [] # should not make any difference
     n = len(Z)
     # make adjacency matrix from linkage matrix
     for bb in Z:
         # connection to the next node/cluster
         n=n+1
         if bb[2] == 0:
-            print("no dist")
             continue
         x,y = (tuple(bb[:-2]))
         adjac.append(tuple([x,n]))
         adjac.append(tuple([y,n]))
-        # print(adjac)
-    #print(adjac)
-    #print(Z)
     
     new_graph.add_edges_from(adjac)
      
@@ -94,8 +120,8 @@ for a in range (len(cutDistance)):
     print("Scores for Cut Distance: ", cutDistance[a])
     print("\n")
     # print sets of nodes, one for each community.
-    print("Communities: ", barbMod)
-    print("\n")
+    # print("Communities: ", barbMod)
+    # print("\n")
     # Modularity Score
     print("Modularity: ", barbModScore)
     print("\n")
@@ -106,8 +132,10 @@ for a in range (len(cutDistance)):
         if clusterL[i] == []:
             # catch Division by zero
             continue
+        if nx.volume(original_graph,clusterL[i]) == 0:
+            continue
         # calculate Cond for current Cluster in list of Clusters 
-        currentCond = conductance(new_graph,clusterL[i])
+        currentCond = conductance(original_graph,clusterL[i])
         sumOfCond.append(currentCond)
         print("Conductance for: ", clusterL[i], " = ", currentCond)
     overallCond = min(sumOfCond)
